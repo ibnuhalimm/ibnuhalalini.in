4e2cf28e21288c9765570ecca4128c288feb4ed0
Ibnu Halim Mustofa <ibnuhalim@pm.me>
2022-05-11 09:19:58 +0700

    fix tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,17 @@ module.exports = {
     "./resources/**/*.vue",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'light-gray': '#DAD5CC',
+        'raisin-black': '#222222',
+        'camel': '#B49B6D',
+        'rich-black': '#01011B',
+        'deep-koamaru': '#303E64',
+        'aero': '#7DBFE1',
+        'water': '#E0F1FD'
+      }
+    },
   },
   plugins: [],
 }
